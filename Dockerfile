043cad0ace6044546f7e4f8c1d2d132ad4b06b29
tongzcomrade <s.rotbamrung@gmail.com>
2021-03-11 23:52:51 +0700

    logging

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,4 @@ COPY . /usr/src/app
 
 RUN yarn install
 
-EXPOSE 3000
-
-CMD [ "npm", "start"]
+CMD [ "node", "index"]
--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const movider = require('./movider')
 const line = require('./alert/line')
 
 cron.schedule('0 * * * *', async () => {
+    console.log('active')
     const { amount } = await movider.getBalance()
     if (amount < 50) {
         line.sendNotify(`เครดิต SMS ใกล้หมดแล้ว จำนวนที่เหลือ $${amount.toFixed(2)}`)
     }
 })
+
+console.log('starting cron')
